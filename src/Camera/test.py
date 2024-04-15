6a2d636b8c21c2089b32e16cb4388ece45177d1e
Gugon11 <gugon.mota@gmail.com>
2024-04-15 19:12:30 +0100

    functions improved

--- src/Camera/test.py
+++ src/Camera/test.py
@@ -52,41 +52,53 @@ def detect_red_car(image):
     # Define the lower and upper bounds for red color in HSV
     lower_red = np.array([0,140,140])
     upper_red = np.array([7,255,255])
+    lower_red2 = np.array([170,150,80])
+    upper_red2 = np.array([180,255,255])
 
     # Create a mask using the specified range
     mask = cv2.inRange(image_hsv, lower_red, upper_red)
+    mask2 = cv2.inRange(image_hsv, lower_red2, upper_red2)
 
     # Perform morphological operations to clean up the mask
     kernel = np.ones((5, 5), np.uint8)
-    mask = cv2.erode(mask, kernel, iterations=1)
+    mask = cv2.erode(mask, kernel, iterations=0)
     mask = cv2.dilate(mask, kernel, iterations=3)
-
+    mask2 = cv2.erode(mask2, kernel, iterations=1)
+    mask2 = cv2.dilate(mask2, kernel, iterations=3)
     # Find contours in the mask
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours2, _ = cv2.findContours(mask2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Filter contours based on area
+    min_area = 500
     max_area = 1400
-    filtered_contours = [cnt for cnt in contours if cv2.contourArea(cnt) < max_area]
+    filtered_contours = [cnt for cnt in contours if min_area < cv2.contourArea(cnt) < max_area]
+    filtered_contours2 = [cnt for cnt in contours2 if min_area < cv2.contourArea(cnt) < max_area]
 
     # Create an empty mask for the filtered contours
     filtered_mask = np.zeros_like(mask)
+    filtered_mask2 = np.zeros_like(mask2)
 
     # Draw the filtered contours on the mask
     cv2.drawContours(filtered_mask, filtered_contours, -1, 255, thickness=cv2.FILLED)
+    cv2.drawContours(filtered_mask2, filtered_contours2, -1, 255, thickness=cv2.FILLED)
 
     # Apply the filtered mask to the original image
-    result = cv2.bitwise_and(image, image, mask=filtered_mask)
+    res = cv2.bitwise_and(image, image, mask=filtered_mask)
+    res2 = cv2.bitwise_and(image, image, mask=filtered_mask2)
 
     # Extract centroids
     moments = cv2.moments(filtered_mask)
     cX = int(moments["m10"] / moments["m00"])
     cY = int(moments["m01"] / moments["m00"])
-
+    
     # Draw centroid on the result image
-    cv2.circle(result, (cX, cY), 10, (255, 255, 255), -1)
-    cv2.putText(result, "centroid", (cX - 25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2)
+    cv2.circle(res, (cX, cY), 10, (255, 255, 255), -1)
+    cv2.putText(res, "centroid", (cX - 25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2)
 
-    return result
+    img = cv2.add(res, res2)
+    
+    return img
 #----------------------------------------------------------------------------------------------------------
 
 
